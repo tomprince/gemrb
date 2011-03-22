50c6088893e22ba3b59e745057a748f8a210d51b
Tom Prince <tom.prince@ualberta.net>
2011-03-22 01:20:17 -0400

    Remove twisted web stuff on this branch.

--- gemrb/GUIScripts/include.py
+++ gemrb/GUIScripts/include.py
@@ -1,9 +1,2 @@
 # this file is executed at gemrb startup, for the Console
 from ie_stats import *
-
-from twisted.internet import reactor
-
-import web
-
-reactor.startRunning()
-GemRB.SetTickHook(lambda: reactor.iterate())
